eef04e314d9b61296f23effe190c071149362e9b
Vinnicius Toth <vinni.toth@gmail.com>
2025-05-17 14:07:02 -0300

    feat: Alterando rota de processamento + documentacao

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-# from routes.producao import get_producao
-from routes.processamento import router as get_processamento
+from fastapi.responses import HTMLResponse
+from routes.welcome import router as welcome_router
+from routes.processamento import router as processamento_router
+from routes.processamento import desc_documentacao as desc_documentacao_processamento
 
-app = FastAPI()
+app = FastAPI(
+    title="API EMBRAPA",
+    description=f"""
+    Rotas válidas:
+    {desc_documentacao_processamento.replace('<b>', '').replace('</b>', '').replace('<br>', '')}
+    """,
+    version="1.0.0"
+)
 
-# app.include_router(get_producao.router)
-app.include_router(get_processamento)
+app.include_router(welcome_router)
+app.include_router(processamento_router)
