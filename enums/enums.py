30c19f80e4c4a6cc6be9fc9d366f07659c5174f0
Vinnicius Toth <vinni.toth@gmail.com>
2025-05-17 11:50:42 -0300

    feat: Adicionando enums

---
+++ enums/enums.py
@@ -0,0 +1,40 @@
+url = "http://vitibrasil.cnpuv.embrapa.br/index.php?"
+
+abas_embrapa = {
+    "producao": {
+        "codigo": "opt_02",
+        "subcategorias": {}
+    },
+    "processamento": {
+        "codigo": "opt_03",
+        "subcategorias": {
+            "viniferas": "subopt_01",
+            "americanas_hibridas": "subopt_02",
+            "uvas_de_mesa": "subopt_03",
+            "sem_classificacao": "subopt_04",
+        }
+    },
+    "comercializacao": {
+        "codigo": "opt_04",
+        "subcategorias": {
+            "viniferas": "subopt_01",
+            "americanas_hibridas": "subopt_02",
+            "uvas_de_mesa": "subopt_03",
+            "sem_classificacao": "subopt_04",
+        }
+    },
+    "importacao": {
+        "codigo": "opt_05",
+        "subcategorias": {
+            "vinhos_de_mesa": "subopt_01",
+            "espumantes": "subopt_02",
+            "uvas_frescas": "subopt_03",
+            "uvas_passas": "subopt_04",
+            "suco_de_uva": "subopt_05",
+        }
+    },
+    "exportacao": {
+        "codigo": "opt_06",
+        "subcategorias": {}
+    }
+}
